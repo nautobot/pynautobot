8085e23f22f56b7dc71302d756152f705fd94701
nautics889 <cyberukr@gmail.com>
2023-04-16 03:37:59 +0300

    fix: Tests for methods for custom fields (#114)
    Add test case `AppCustomFieldChoicesTestCase` for
    `custom_field_choices()` method.
    Rename test case for `custom_fields()` method.
    Add using fixtures for mentioned test cases.

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,8 @@ from unittest.mock import patch
 
 import pynautobot
 
+from .util import Response
+
 host = "http://localhost:8000"
 
 def_kwargs = {
@@ -10,16 +12,51 @@ def_kwargs = {
 }
 
 
-class AppCustomChoicesTestCase(unittest.TestCase):
+class AppCustomFieldsTestCase(unittest.TestCase):
     @patch(
-        "pynautobot.core.query.Request.get",
-        return_value={"Testfield1": {"TF1_1": 1, "TF1_2": 2}, "Testfield2": {"TF2_1": 3, "TF2_2": 4}},
+        "requests.sessions.Session.get",
+        side_effect=[Response(fixture="extras/custom_fields.json")],
     )
-    def test_custom_choices(self, *_):
+    def test_custom_fields(self, session_get_mock):
         api = pynautobot.api(host, **def_kwargs)
         choices = api.extras.custom_fields()
+
+        session_get_mock.assert_called_once()
+        expect_url = f"{api.base_url}/extras/custom-fields/"
+        url_passed_in_args = expect_url in session_get_mock.call_args.args
+        url_passed_in_kwargs = expect_url == session_get_mock.call_args.kwargs.get("url")
+        self.assertTrue(url_passed_in_args or url_passed_in_kwargs)
+
+        self.assertIsInstance(choices, list)
         self.assertEqual(len(choices), 2)
-        self.assertEqual(sorted(choices.keys()), ["Testfield1", "Testfield2"])
+        for field in choices:
+            self.assertIsInstance(field.get("name"), str)
+            self.assertIsInstance(field.get("content_types"), list)
+            self.assertIsInstance(field.get("slug"), str)
+            self.assertIn("type", field)
+
+
+class AppCustomFieldChoicesTestCase(unittest.TestCase):
+    @patch(
+        "requests.sessions.Session.get",
+        side_effect=[Response(fixture="extras/custom_field_choices.json")],
+    )
+    def test_custom_field_choices(self, session_get_mock):
+        api = pynautobot.api(host, **def_kwargs)
+        choices = api.extras.custom_field_choices()
+
+        session_get_mock.assert_called_once()
+        expect_url = f"{api.base_url}/extras/custom-field-choices/"
+        url_passed_in_args = expect_url in session_get_mock.call_args.args
+        url_passed_in_kwargs = expect_url == session_get_mock.call_args.kwargs.get("url")
+        self.assertTrue(url_passed_in_args or url_passed_in_kwargs)
+
+        self.assertIsInstance(choices, list)
+        self.assertEqual(len(choices), 3)
+        for choice in choices:
+            self.assertIsInstance(choice.get("field"), dict)
+            self.assertIsInstance(choice.get("value"), str)
+            self.assertIn(choice.get("value"), ("First option", "Second option", "Third option"))
 
 
 class AppConfigTestCase(unittest.TestCase):
