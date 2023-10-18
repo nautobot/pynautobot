a4ae01540fb02a8d7653d43ea2b08072597b7a6a
pszulczewski <patryk@szulczewski.eu>
2023-10-18 13:21:44 +0200

    Fix custom_fields and custom_field_choices method overlap with endpoints (#141)

---
+++ tests/integration/test_extras.py
@@ -0,0 +1,36 @@
+import pytest
+
+
+class TestCustomField:
+
+    """Verify we can create, custom field, and custom field choices."""
+
+    @pytest.fixture(scope="session")
+    def create_custom_field(self, nb_client):
+        data = {
+            "label": "test_cf",
+            "key": "test_cf",
+            "content_types": ["dcim.device"],
+            "type": "select",
+            "weight": 100,
+            "filter_logic": "loose",
+        }
+        return nb_client.extras.custom_fields.create(**data)
+
+    @pytest.fixture(scope="session")
+    def create_custom_field_choices(self, nb_client, create_custom_field):
+        data = {
+            "value": "A",
+            "custom_field": create_custom_field["id"],
+            "weight": 100,
+        }
+        return nb_client.extras.custom_field_choices.create(**data)
+
+    def test_custom_field(self, create_custom_field):
+        assert create_custom_field["label"] == "test_cf"
+
+    def test_custom_field_choice(self, create_custom_field_choices):
+        assert create_custom_field_choices["value"] == "A"
+
+    def test_custom_field_choice_to_cf(self, create_custom_field_choices, create_custom_field):
+        assert create_custom_field_choices["custom_field"]["id"] == create_custom_field["id"]
