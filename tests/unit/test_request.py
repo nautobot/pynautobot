30ad305cf8c56718248691d6c301ca011a530d9f
Joe Wesch <joe.wesch@networktocode.com>
2024-03-26 19:33:16 -0500

    Fixes test

--- tests/unit/test_request.py
+++ tests/unit/test_request.py
@@ -6,9 +6,9 @@ from pynautobot.core.query import Request
 
 class RequestTestCase(unittest.TestCase):
     def test_get_openapi(self):
-        test = Request("http://localhost:8080/api", Mock())
+        test = Request("http://localhost:8080/api", Mock(), token="1234")
         test.get_openapi()
         test.http_session.get.assert_called_with(
             "http://localhost:8080/api/docs/?format=openapi",
-            headers={"Content-Type": "application/json;"},
+            headers={"Content-Type": "application/json;", "Authorization": "Token 1234"},
         )
