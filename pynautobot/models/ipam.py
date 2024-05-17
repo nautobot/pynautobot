bb3652b01bdad3cdd769f217536b82c3c60d8339
Josh VanDeraa <jv@networktocode.com>
2024-05-17 15:58:08 -0500

    Fixes docs of what it should be.

--- pynautobot/models/ipam.py
+++ pynautobot/models/ipam.py
@@ -31,62 +31,64 @@ class Prefixes(Record):
 
     @property
     def available_ips(self):
-        """Represents the ``available-ips`` detail endpoint.
+        """
+        Represents the ``available-ips`` detail endpoint.
 
         Returns a DetailEndpoint object that is the interface for
         viewing and creating IP addresses inside a prefix.
 
-        :returns: :py:class:`.DetailEndpoint`
+        Returns:
+            DetailEndpoint: The detail endpoint interface for available IPs.
 
-        :Examples:
+        Examples:
+            List available IPs:
 
-        >>> prefix = nb.ipam.prefixes.get(24)
-        >>> prefix.available_ips.list()
-        [{u'vrf': None, u'family': 4, u'address': u'10.1.1.49/30'}...]
+            >>> prefix = nb.ipam.prefixes.get(prefix="10.0.192.0/18")
+            >>> prefix.available_ips.list()
+            [<pynautobot.models.ipam.IpAddresses ('10.0.192.1/18') at 0x103cb59d0>, <pynautobot.models.ipam.IpAddresses ('10.0.192.2/18') at 0x103cefb90>,...]
 
-        To create a single IP:
+            To create a single IP:
 
-        >>> prefix = nb.ipam.prefixes.get(24)
-        >>> prefix.available_ips.create()
-        {u'status': 1, u'description': u'', u'nat_inside': None...}
+            >>> prefix = nb.ipam.prefixes.get(prefix="10.0.192.0/18")
+            >>> prefix.available_ips.create()
+            {u'status': 1, u'description': u'', u'nat_inside': None...}
 
+            To create multiple IPs:
 
-        To create multiple IPs:
-
-        >>> prefix = nb.ipam.prefixes.get(24)
-        >>> create = prefix.available_ips.create([{} for i in range(2)])
-        >>> len(create)
-        2
+            >>> prefix = nb.ipam.prefixes.get(prefix="10.0.192.0/18")
+            >>> create = prefix.available_ips.create([{} for i in range(2)])
+            >>> len(create)
+            2
         """
+
         return DetailEndpoint(self, "available-ips", custom_return=IpAddresses)
 
     @property
     def available_prefixes(self):
-        """Represents the ``available-prefixes`` detail endpoint.
+        """
+        Represents the ``available-prefixes`` detail endpoint.
 
         Returns a DetailEndpoint object that is the interface for
         viewing and creating prefixes inside a parent prefix.
 
-        Very similar to :py:meth:`~pynautobot.ipam.Prefixes.available_ips`
-        , except that dict (or list of dicts) passed to ``.create()``
-        needs to have a ``prefix_length`` key/value specifed.
+        Very similar to :py:meth:`~pynautobot.ipam.Prefixes.available_ips`,
+        except that the dict (or list of dicts) passed to ``.create()`` 
+        needs to have a ``prefix_length`` key/value specified.
 
-        :returns: :py:class:`.DetailEndpoint`
+        Returns:
+            DetailEndpoint: The detail endpoint interface for available prefixes.
 
-        :Examples:
+        Examples:
+            List available prefixes:
 
-        >>> prefix.available_prefixes.list()
-        [{u'prefix': u'10.1.1.44/30', u'vrf': None, u'family': 4}]
+            >>> prefix.available_prefixes.list()
+            [{u'prefix': u'10.1.1.44/30', u'vrf': None, u'family': 4}]
 
+            Creating a single child prefix:
 
-        Creating a single child prefix:
-
-        >>> prefix = nb.ipam.prefixes.get(1)
-        >>> new_prefix = prefix.available_prefixes.create(
-        ...    {'prefix_length': 29}
-        ...)
-        >>> new_prefix['prefix']
-        u'10.1.1.56/29'
-
+            >>> prefix = nb.ipam.prefixes.get(prefix="10.0.192.0/18")
+            >>> new_prefix = prefix.available_prefixes.create({'prefix_length': 29, 'status': 'Active'})
+            >>> new_prefix.prefix
+            '10.0.192.40/29'
         """
         return DetailEndpoint(self, "available-prefixes", custom_return=Prefixes)
